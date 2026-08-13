3a6acfe44ea73a5ca5ab7d5e0af307fb49096581
kunalraj0212 <razzkunal7@gmail.com>
2026-08-13 12:17:26 +0530

    Add my solution for Python arithmetic operators

---
+++ 01-python-fundamentals/00-python-basics/06-arithmetic-operators.py
@@ -0,0 +1,13 @@
+# 06. Arithmetic Operators in Python
+
+a = 15
+b = 4
+
+print(f"a = {a}, b = {b}\n")
+print(f"Addition        (a + b)  : {a + b}")
+print(f"Subtraction     (a - b)  : {a - b}")
+print(f"Multiplication  (a * b)  : {a * b}")
+print(f"Division (float)(a / b)  : {a / b}")
+print(f"Floor Division  (a // b) : {a // b}  (drops decimal part)")
+print(f"Modulus/Remainder(a % b) : {a % b}   (remainder after division)")
+print(f"Exponentiation  (a ** b) : {a ** b}  (15 raised to power 4)")
